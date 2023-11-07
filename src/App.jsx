b78c133458992dc6a855cbcd548999736907e15f
Oleg8995 <oleg8995@gmail.com>
2023-11-07 23:23:44 +0200

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Main_container } from './sections/main_container/main_container'
 
 
 function App() {
   
   return (
     <div>
-      
+      <Main_container/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './sections/content_section'
-import {Main_container} from './sections/main_container'
+// import {Main_container} from './sections/main_container'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Main_container/>
+    {/* <Main_container/> */}
   </React.StrictMode>,
 )
